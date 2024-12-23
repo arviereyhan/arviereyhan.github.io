83f8d078b0afa7e60dbdc4a41198739fc9b70870
arvie-reyhan <reyhan.arvie@gmail.com>
2024-12-23 22:36:35 +0700

    Replace all content with the 'assets' folder from iqbalportofolio repository

---
+++ js/main.js
@@ -0,0 +1,30 @@
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+ 
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+document.addEventListener("click",function (e){
+  if(e.target.classList.contains("gallery-item")){
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+      myModal.show();
+  }
+})
+
+window.addEventListener('scroll',() => {
+  document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+})
